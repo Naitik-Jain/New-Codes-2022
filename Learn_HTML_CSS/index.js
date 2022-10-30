542b4f605654cb479d585edec66516bc0f2d8166
Mr.Incredible.29 <65172813+Naitik-Jain@users.noreply.github.com>
2022-10-30 23:01:48 +0530

    Delete Learn_HTML_CSS directory

--- Learn_HTML_CSS/index.js
+++
@@ -1,7 +0,0 @@
-function showTooltip() {
-    document.getElementById('tooltip-for-dob').className = 'tooltip-for-dob-show';
-  }
-  
-  function hideTooltip() {
-    document.getElementById('tooltip-for-dob').className = 'tooltip-for-dob-hide';
-  }
