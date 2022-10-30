efe15bd363fa6fec08bfac3aa438272a5d59e55b
Mr.Incredible.29 <65172813+Naitik-Jain@users.noreply.github.com>
2022-10-30 23:55:26 +0530

    Add files via upload

---
+++ enter single char.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+	int ch;
+	printf("enter single character");
+	ch=getchar();
+	printf("you have enter");
+	putchar(ch);
+	
+}
---
+++ escape 2 variable.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+main()
+{
+	int a=1,b=200;
+	b=a;
+	printf("a&b %d %d",a,b);
+} 
